72e477f8b437e4aa453bc70ecf293b5ceacc0316
RostNshka <devkotder@ya.ru>
2026-03-25 19:19:01 +0400

    added validation text

--- src/vacancy/vacancy.controller.js
+++ src/vacancy/vacancy.controller.js
@@ -6,6 +6,9 @@ const router = Router()
 const vacancyService = new VacancyService()
 
 router.post('/', (req, res) => {
+  if (!req.body?.text?.length) {
+    return res.status(400).json({ message: 'Text is required' })
+  }
   const vacancy = vacancyService.createVacancy(req.body)
   return res.status(200).json(vacancy)
 })
