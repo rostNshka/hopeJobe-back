0e646e63486eae6bed1559dfd8810fa1053cb5f3
RostNshka <devkotder@ya.ru>
2026-03-26 21:25:25 +0400

    minifix

--- src/auth.middleware.js
+++ src/auth.middleware.js
@@ -1,8 +1,11 @@
 export const auth = async (req, res, next) => {
   const authHeader = req.headers['authorization']
   if (!authHeader) {
-    return res.status(401).json({ message: 'Unauthorized' })
-
-    next()
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+  try {
+    next(); // вызываем next после успешной проверки
+  } catch (error) {
+    return res.status(403).json({ message: 'Invalid token' });
   }
 }
