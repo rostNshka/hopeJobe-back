d87b862d7241a4c971d1d8a3c4e2967d0aeab5a4
rostNshka <devkotder@ya.ru>
2026-03-31 19:54:32 +0400

    change validation.js

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -8,7 +8,23 @@ const validatePassword = (password) => {
   return password && password.length >= 6
 }
 
+const validatePhone = (phone) => {
+  const phoneRegex = /^\+?[\d\s-]{10,}$/
+  return phoneRegex.test(phone)
+};
+
+const validateUrl = (url) => {
+  try {
+    new URL(url)
+    return true
+  } catch {
+    return false
+  }
+}
+
 module.exports = {
   validateEmail,
   validatePassword,
+  validatePhone,
+  validateUrl
 }
